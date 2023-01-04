e9e792f9cf21d853755ad0c13d22c6226a08b954
takahiro-okada <okahanako09875@gmail.com>
2023-01-05 06:49:04 +0900

    feat:戻り値の処理

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Call method");
-    hello();
-  }
 
-  public static void hello(){
-    System.out.println("Hi oka");
+  public static void main(String[] args) {
+    int ans = add(100, 10);
+    System.out.println(ans);
+  }
+  public static int add(int x, int y){
+    int ans = x + y;
+    return ans;
   }
 }
