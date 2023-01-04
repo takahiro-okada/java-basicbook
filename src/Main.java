5872151ce5d9748023d93d597f22f5ad0a22b8fc
takahiro-okada <okahanako09875@gmail.com>
2023-01-05 05:52:35 +0900

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Okada");
+    System.out.println(31 + 31);
+    int x;
+    x = 6;
+    System.out.println(x * x * 3.14);
+  }
+}
