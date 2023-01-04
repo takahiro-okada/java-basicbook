c51669bbbbb2eafafe07a709578e088fa3886abd
takahiro-okada <okahanako09875@gmail.com>
2023-01-05 06:30:58 +0900

    feat:メソッドの作成と呼び出し

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 public class Main {
   public static void main(String[] args) {
-    int[] scores = {20, 30, 40, 50, 80};
-    for (int i =0; i < scores.length; i++){
-      System.out.println(scores[i]);
-    }
+    System.out.println("Call method");
+    hello();
+  }
 
+  public static void hello(){
+    System.out.println("Hi oka");
   }
 }
