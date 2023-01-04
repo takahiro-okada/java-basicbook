f3259530d30c87b619155a7161f86355e719cda8
takahiro-okada <okahanako09875@gmail.com>
2023-01-05 06:06:33 +0900

    feat:変数の初期化

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,7 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Okada");
     System.out.println(31 + 31);
-    int x;
-    x = 6;
-    System.out.println(x * x * 3.14);
+    int x = 6;
+    System.out.println(x);
   }
 }
